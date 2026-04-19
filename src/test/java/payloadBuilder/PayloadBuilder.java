e624bc5820d164ca34940ece2d85fb2736c4ec44
mnazo <monwa.nazo@gmail.com>
2026-04-19 20:17:23 +0200

    added payloadBuilder

--- src/test/java/payloadBuilder/PayloadBuilder.java
+++ src/test/java/payloadBuilder/PayloadBuilder.java
@@ -1,5 +1,29 @@
 package payloadBuilder;
 
+
+import org.json.simple.JSONObject;
+
 public class PayloadBuilder {
+
+    public static JSONObject userLoginPayload(String email, String password) {
+
+        JSONObject userLogin = new JSONObject();
+        userLogin.put("email", email);
+        userLogin.put("password", password);
+
+        return userLogin;
+    }
+
+    public static JSONObject userRegistrationPayload(String firstName, String lastName, String email, String password, String groupId) {
+        JSONObject userRegistration = new JSONObject();
+        userRegistration.put("firstName", firstName);
+        userRegistration.put("lastName", lastName);
+        userRegistration.put("email", email);
+        userRegistration.put("password", password);
+        userRegistration.put("confirmPassword", password);
+        userRegistration.put("groupId", groupId);
+
+        return userRegistration;
+    }
 
 }
